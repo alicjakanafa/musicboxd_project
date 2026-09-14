062d22f3c176f9356be4d7842ccc4c074f99799a
CorbanAllan <34634236+CorbanAllan@users.noreply.github.com>
2026-09-14 14:46:52 +0100

    spring boot init

---
+++ MusicBoxd/src/main/java/com/example/MusicBoxd/MusicBoxdApplication.java
@@ -0,0 +1,13 @@
+package com.example.MusicBoxd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicBoxdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicBoxdApplication.class, args);
+	}
+
+}
