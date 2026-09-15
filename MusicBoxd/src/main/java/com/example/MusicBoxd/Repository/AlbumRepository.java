21d84197c5c24321a46ebbebf37a070f1ee457da
eboyle26 <291289640+eboyle26@users.noreply.github.com>
2026-09-15 20:21:00 +0100

    HTML updated to include spotify

--- MusicBoxd/src/main/java/com/example/MusicBoxd/Repository/AlbumRepository.java
+++ MusicBoxd/src/main/java/com/example/MusicBoxd/Repository/AlbumRepository.java
@@ -3,5 +3,12 @@ package com.example.MusicBoxd.Repository;
 import com.example.MusicBoxd.Model.Album;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AlbumRepository extends CrudRepository<Album,Long> {
+import java.util.List;
+import java.util.Optional;
+
+public interface AlbumRepository extends CrudRepository<Album, Long> {
+
+    List<Album> findByArtistId(Long artistId);
+
+    Optional<Album> findByExternalId(String externalId);
 }
