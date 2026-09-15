0db46e18b2a193f1ce12e65d9dcf4c8b2341034f
eboyle26 <291289640+eboyle26@users.noreply.github.com>
2026-09-15 16:11:24 +0100

    fixed case issue

--- MusicBoxd/src/main/java/com/example/MusicBoxd/Controller/lastFmController.java
+++ MusicBoxd/src/main/java/com/example/MusicBoxd/Controller/lastFmController.java
@@ -1,4 +1,4 @@
-package com.example.MusicBoxd.controller;
+package com.example.MusicBoxd.Controller;
 
 import com.example.MusicBoxd.api.lastfm.LastFmResponse;
 import com.example.MusicBoxd.api.lastfm.LastFmService;
