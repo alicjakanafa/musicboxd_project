ba651c16fd940d47de0cf8e3d99863f142e88f70
CorbanAllan <34634236+CorbanAllan@users.noreply.github.com>
2026-09-15 12:05:32 +0100

    minor bug fixes to database migrations

--- MusicBoxd/src/main/resources/db/migration/V1__init.sql
+++ MusicBoxd/src/main/resources/db/migration/V1__init.sql
@@ -1,5 +1,3 @@
-TRUNCATE TABLE IF EXISTS users;
-
 CREATE TABLE users (
    id bigserial PRIMARY KEY,
    google_user_id VARCHAR(255) UNIQUE,
@@ -7,5 +5,5 @@ CREATE TABLE users (
    email VARCHAR(255),
    bio TEXT,
    profile_picture_url TEXT,
-   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
--- MusicBoxd/src/main/resources/db/migration/V4__create_albums_table.sql
+++ MusicBoxd/src/main/resources/db/migration/V4__create_albums_table.sql
@@ -1,11 +1,9 @@
-TRUNCATE TABLE IF EXISTS albums;
-
 CREATE TABLE albums (
    id bigserial PRIMARY KEY,
    external_id VARCHAR(255),
    artist_id BIGINT,
    title varchar(255),
-   release_year YEAR,
+   release_year SMALLINT,
    artwork_url TEXT,
-   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
