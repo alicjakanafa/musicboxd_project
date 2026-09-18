2bb2f3dbbe26cc68883f335d5c0c4ce46cf9cb42
CorbanAllan <34634236+CorbanAllan@users.noreply.github.com>
2026-09-18 11:37:45 +0100

    added list type field to lists table to eliminate the need for want to listen and favourites tables.Also both lists will be automatically created for users upon account creation

--- MusicBoxd/src/main/resources/db/migration/V20__create_favourite_albums.sql
+++ MusicBoxd/src/main/resources/db/migration/V20__create_favourite_albums.sql
@@ -1,20 +1,20 @@
 CREATE TABLE user_favourite_albums (
-                                       id BIGSERIAL PRIMARY KEY,
-                                       user_id BIGINT NOT NULL,
-                                       album_id BIGINT NOT NULL,
-                                       position INTEGER NOT NULL,
+   id BIGSERIAL PRIMARY KEY,
+   user_id BIGINT NOT NULL,
+   album_id BIGINT NOT NULL,
+   position INTEGER NOT NULL,
 
-                                       CONSTRAINT fk_favourite_user
-                                           FOREIGN KEY (user_id)
-                                               REFERENCES users(id),
+   CONSTRAINT fk_favourite_user
+       FOREIGN KEY (user_id)
+           REFERENCES users(id),
 
-                                       CONSTRAINT fk_favourite_album
-                                           FOREIGN KEY (album_id)
-                                               REFERENCES albums(id),
+   CONSTRAINT fk_favourite_album
+       FOREIGN KEY (album_id)
+           REFERENCES albums(id),
 
-                                       CONSTRAINT unique_user_album
-                                           UNIQUE (user_id, album_id),
+   CONSTRAINT unique_user_album
+       UNIQUE (user_id, album_id),
 
-                                       CONSTRAINT unique_user_position
-                                           UNIQUE (user_id, position)
+   CONSTRAINT unique_user_position
+       UNIQUE (user_id, position)
 );
