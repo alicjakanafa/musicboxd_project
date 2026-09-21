baa5015be03b636a4e672bc9fbcf7fc9186c7634
BenMcCusker <ben_mccusker@yahoo.co.uk>
2026-09-21 14:26:03 +0100

    Added messaging functionality over from Acebook project

--- MusicBoxd/src/main/java/com/example/MusicBoxd/Repository/FriendRepository.java
+++ MusicBoxd/src/main/java/com/example/MusicBoxd/Repository/FriendRepository.java
@@ -1,6 +1,7 @@
 package com.example.MusicBoxd.Repository;
 
 import com.example.MusicBoxd.Model.Friend;
+import com.example.MusicBoxd.Model.User;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -31,5 +32,17 @@ public interface FriendRepository
 
     List<Friend> findByStatus(
             String status
+    );
+
+    boolean existsByReceiverAndRequesterAndStatus(
+            User receiver,
+            User requester,
+            Friend.Status status
+    );
+
+    boolean existsByRequesterAndReceiverAndStatus(
+            User requester,
+            User receiver,
+            Friend.Status status
     );
 }
